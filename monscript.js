25058d1e0a91d5ae2e8cfd470a84e3258ec84633
Fanfnix <valentin@familledenis.net>
2026-02-04 11:17:09 +0100

    TP4

--- monscript.js
+++ monscript.js
@@ -1,3 +1,13 @@
 // alert("Ceci vient de monfichier JS !");
 // let reponse = prompt('Quelle est ta série préférée ?');
 // console.log(reponse);
+
+let nomDuJour = "Mercredi";
+let numeroDuJour = 2;
+let nomDUMois = "Février";
+let isHiver = true;
+
+console.log(nomDuJour);
+console.log(numeroDuJour);
+console.log(nomDUMois);
+console.log(isHiver);
