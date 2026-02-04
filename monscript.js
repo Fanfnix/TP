b821edafdb7520b68974bdf99c3ae5b2572f5e05
Fanfnix <valentin@familledenis.net>
2026-02-04 11:39:24 +0100

    TP7

--- monscript.js
+++ monscript.js
@@ -26,9 +26,14 @@ nbr2 = 7;
 nbr3 = 8;
 nbr4 = 21;
 
-console.log(nbr1 + nbr2);
-console.log(nbr4 - nbr3);
-console.log(nbr2 = nbr2 * nbr1);
-console.log(nbr2 += 1);
-console.log(nbr4 /= nbr1);
-console.log(nbr4 % nbr1);
+// console.log(nbr1 + nbr2);
+// console.log(nbr4 - nbr3);
+// console.log(nbr2 = nbr2 * nbr1);
+// console.log(nbr2 += 1);
+// console.log(nbr4 /= nbr1);
+// console.log(nbr4 % nbr1);
+
+let nomSerie, nomPerso;
+nomSerie = prompt("Quelle est ta série préféré ?");
+nomPerso = prompt("Quel est ton personnage favori ?")
+alert(`Ma série préféré est ${nomSerie} et mon personnage favori est ${nomPerso}.`);
